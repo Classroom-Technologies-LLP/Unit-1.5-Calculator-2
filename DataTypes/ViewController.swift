6fa9c06b4aeb68863162cdd70a7f0fe23b837cdc
Sim Yong Seng <simyseng@gmail.com>
2019-09-24 22:10:48 +0700

    Challenge completed

--- DataTypes/ViewController.swift
+++ DataTypes/ViewController.swift
@@ -9,16 +9,87 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var textField: UITextField!
-    @IBAction func runButtonTapped(_ sender: Any) {
-        guard let textFieldString = textField.text else {return}
-        guard let textFieldNumber = Int(textFieldString) else {
-            print("Text is not convertible to integer")
-            return            
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        // Get text property from textField1 and convert to Double
+        guard let textField1String = textField1.text else {return}
+        guard let textField1Number = Double(textField1String) else {
+            print("Text is not convertible to Double")
+            return
         }
+        
+        // Get text property from textField2 and convert to Double
+        guard let textField2String = textField2.text else {return}
+        guard let textField2Number = Double(textField2String) else {
+            print("Text is not convertible to Double")
+            return
+        }
+        
+        // Both text fields' text properties can be converted to Double, so add them
+        print(textField1Number + textField2Number)
 
-        print(textFieldString)
-        print(textFieldNumber)
+    }
+    
+    @IBAction func subButtonTapped(_ sender: Any) {
+        // Get text property from textField1 and convert to Double
+        guard let textField1String = textField1.text else {return}
+        guard let textField1Number = Double(textField1String) else {
+            print("Text is not convertible to Double")
+            return
+        }
+        
+        // Get text property from textField2 and convert to Double
+        guard let textField2String = textField2.text else {return}
+        guard let textField2Number = Double(textField2String) else {
+            print("Text is not convertible to Double")
+            return
+        }
+        
+        // Both text fields' text properties can be converted to Double, so add them
+        print(textField1Number - textField2Number)
+        
+    }
+    
+    @IBAction func mulButtonTapped(_ sender: Any) {
+        // Get text property from textField1 and convert to Double
+        guard let textField1String = textField1.text else {return}
+        guard let textField1Number = Double(textField1String) else {
+            print("Text is not convertible to Double")
+            return
+        }
+        
+        // Get text property from textField2 and convert to Double
+        guard let textField2String = textField2.text else {return}
+        guard let textField2Number = Double(textField2String) else {
+            print("Text is not convertible to Double")
+            return
+        }
+        
+        // Both text fields' text properties can be converted to Double, so add them
+        print(textField1Number * textField2Number)
+        
+    }
+    
+    @IBAction func divButtonTapped(_ sender: Any) {
+        // Get text property from textField1 and convert to Double
+        guard let textField1String = textField1.text else {return}
+        guard let textField1Number = Double(textField1String) else {
+            print("Text is not convertible to Double")
+            return
+        }
+        
+        // Get text property from textField2 and convert to Double
+        guard let textField2String = textField2.text else {return}
+        guard let textField2Number = Double(textField2String) else {
+            print("Text is not convertible to Double")
+            return
+        }
+        
+        // Both text fields' text properties can be converted to Double, so add them
+        print(textField1Number / textField2Number)
+        
     }
     
     override func viewDidLoad() {
